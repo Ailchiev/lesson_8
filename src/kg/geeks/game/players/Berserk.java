47a395132b9d9a7b0d2eefd4ccf3906502af3246
Uluki <ailchievulugbek@gmail.com>
2023-03-27 22:33:37 +0600

    Initial commit

---
+++ src/kg/geeks/game/players/Berserk.java
@@ -0,0 +1,17 @@
+package kg.geeks.game.players;
+
+public class Berserk extends Hero {
+    private int blockedDamage;
+
+    public Berserk(int health, int damage, String name) {
+        super(health, damage, SuperAbility.SAVE_DAMAGE_AND_REVERT, name);
+    }
+
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+
+    }
+
+    public void setBlockedDamage(int blockedDamage) {
+        this.blockedDamage = blockedDamage;
+    }
+}
