f67ceb8f1240315d6b27c3250d816bbc79d44929
Uluki <ailchievulugbek@gmail.com>
2023-03-27 23:33:27 +0600

    Added changes

--- src/kg/geeks/game/players/Berserk.java
+++ src/kg/geeks/game/players/Berserk.java
@@ -6,7 +6,7 @@ public class Berserk extends Hero {
     public Berserk(int health, int damage, String name) {
         super(health, damage, SuperAbility.SAVE_DAMAGE_AND_REVERT, name);
     }
-
+      //оооо
     public void applySuperPower(Boss boss, Hero[] heroes) {
         this.setDamage(this.getDamage()+(boss.getDamage()/3));
     }
